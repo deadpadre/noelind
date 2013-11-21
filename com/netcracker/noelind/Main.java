77de09f43bc6b4236288ff619ae8ac0543c646c1
deadpadre <tkachenoko@gmail.com>
2013-11-21 21:33:54 +0400

    Testing commiting via IDEA.

--- com/netcracker/noelind/Main.java
+++ com/netcracker/noelind/Main.java
@@ -2,7 +2,7 @@ package com.netcracker.klenin.noelind;
 
 public class Main {
 	public static void main(String [] args) {
-		User A = new User("Kolya");
+		User A = new User("Vasya");
 		A.writePost("123");
 		
 	}
